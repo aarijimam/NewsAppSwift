aa8640e1e6181267d68ba248861cd58465643fe8
aarijimam <35100854+aarijimam@users.noreply.github.com>
2024-05-25 23:15:30 +0500

    Added Tab View

--- NewsApp/Views/ErrorView.swift
+++ NewsApp/Views/ErrorView.swift
@@ -8,53 +8,11 @@
 import SwiftUI
 
 struct ErrorView: View {
-    
-    typealias ErrorViewActionHandler = () -> Void
-    
-    let error:Error
-    let handler: ErrorViewActionHandler
-    
-    internal init(error: any Error, 
-                  handler: @escaping ErrorView.ErrorViewActionHandler) {
-        self.error = error
-        self.handler = handler
-    }
-    
-    
     var body: some View {
-        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/){
-            Spacer()
-            VStack{
-                Image(systemName: "exclamationmark.icloud.fill")
-                    .foregroundColor(.red)
-                    .font(.system(size: 50, weight: .heavy))
-                Text("Ooops!")
-                    .foregroundStyle(.black)
-                    .font(.system(size: 30, weight: .heavy))
-                Text(error.localizedDescription)
-                    .foregroundStyle(Color.gray)
-                    .font(.system(size: 15,weight: .light))
-                    .multilineTextAlignment(.center)
-                    .padding(.vertical, 2)
-            }
-            .padding(.top, 200)
-            Spacer()
-            Button(action:{
-                handler()
-            },label: {
-                Text("Retry")
-            })
-            .padding(.vertical, 15)
-            .padding(.horizontal, 30)
-            .background(Color.blue)
-            .foregroundStyle(.white)
-            .font(.system(size: 15,weight: .heavy))
-            .cornerRadius(10)
-            .frame(height: 300)
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ErrorView(error: APIError.decodingError) {}
+    ErrorView()
 }
