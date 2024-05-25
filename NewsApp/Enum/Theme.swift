2f087e58990e70c27d7a3f48e7f7087c0de4e1f6
aarijimam <35100854+aarijimam@users.noreply.github.com>
2024-05-26 01:18:09 +0500

    Added dark mode support, and favourites tab

---
+++ NewsApp/Enum/Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Theme.swift
+//  NewsApp
+//
+//  Created by Aarij Imam on 26/05/2024.
+//
+
+import Foundation
---
+++ NewsApp/Views/FavouritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavouritesView.swift
+//  NewsApp
+//
+//  Created by Aarij Imam on 26/05/2024.
+//
+
+import SwiftUI
+
+struct FavouritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavouritesView()
+}
